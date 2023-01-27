9427b514d35329c3d3e8cdad6b1eb140d8c5cbc7
j1-y00n <121421760+j1-y00n@users.noreply.github.com>
2023-01-27 15:33:53 +0900

    [Bronze II] Title: 나머지, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.py
+++ 백준/Bronze/3052. 나머지/나머지.py
@@ -1,9 +1,3 @@
-li = [int(input()) for i in range(10)]
-li2 = [j % 42 for j in li]
-d = {}
-for m in li2:
-    if m not in d:
-        d[m] = 1
-    else:
-        d[m] += 1
-print(len(d))
+num = [int(input()) for i in range(10)]
+s = {j % 42 for j in num}
+print(len(s))
