1a5d383d28eced58bbf1bc3e5cba9c3e0fccf13e
j1-y00n <121421760+j1-y00n@users.noreply.github.com>
2023-02-01 01:08:00 +0900

    [Bronze III] Title: 최댓값, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.py
+++ 백준/Bronze/2566. 최댓값/최댓값.py
@@ -2,10 +2,12 @@ import sys
 
 num = [list(map(int, sys.stdin.readline().split())) for _ in range(9)]
 
-max_num = max(sum(num, []))
-print(max(sum(num, [])))
-
+li = []
+max_num = -1
 for i in range(9):
     for j in range(9):
-        if num[i][j] == max_num:
-            print(i + 1, j + 1)
+        if num[i][j] > max_num:
+            max_num = num[i][j]
+            li.append((i + 1, j + 1))
+print(max_num)      
+print(*li.pop())
