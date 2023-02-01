77cb0c6308c6d73832c3184c019946b2f50fae3c
j1-y00n <121421760+j1-y00n@users.noreply.github.com>
2023-02-01 15:14:57 +0900

    [Bronze II] Title: 점수 집계, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/9076. 점수 집계/점수 집계.py
+++ 백준/Bronze/9076. 점수 집계/점수 집계.py
@@ -1,12 +1,9 @@
 import sys
-from collections import deque
 
 for _ in range(int(input())):
     score = list(map(int, sys.stdin.readline().split()))
-    score2 = deque(sorted(score))
-    score2.pop()
-    score2.popleft()
-    if score2[2] - score2[0] >= 4:
+    score2 = sorted(score)
+    if score2[3] - score2[1] >= 4:
         print('KIN')
     else:
-        print(sum(score2))
+        print(sum(score2[1:4]))
