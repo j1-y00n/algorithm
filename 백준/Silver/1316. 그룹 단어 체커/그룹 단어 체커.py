04d2529f57001a6132a0f743bc636381e19e25a5
j1-y00n <121421760+j1-y00n@users.noreply.github.com>
2023-02-04 22:14:25 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -1,23 +1,12 @@
 import sys
 
-group = 0
 N = int(input())
+group = N
 for _ in range(N):
     word = sys.stdin.readline().strip()
-    if len(word) == 1:
-        group += 1
-    
-    else:
-        check = []
-        w = word[0]
-        for i in range(1, len(word)):
-            if w != word[i]:
-                check.append(w)
-                w = word[i]
-        check.append(word[i])
-        
-        s_word = set(word)
-        if len(check) == len(s_word):
-            group += 1
 
+    for i in range(len(word)-1):
+        if word[i] != word[i + 1] and word[i] in word[i + 1:]:
+            group -= 1
+            break
 print(group)
